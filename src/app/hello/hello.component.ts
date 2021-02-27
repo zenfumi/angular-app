407fbabb512d31bc5c9228f20bd574d85c038385
zenfumi <ys00878@gmail.com>
2021-02-27 23:51:57 +0900

    fix

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -2,18 +2,31 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
-  templateUrl: './hello.component.html',
-  styleUrls: ['./hello.component.css']
+  styleUrls: ['./hello.component.css'],
+  templateUrl: './hello.component.html'
 })
+
 export class HelloComponent implements OnInit {
   title:string;
   message:string;
+  nowStyle:any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-    this.title = 'Hello-app'
-    this.message = 'This is My First Component!!'
-    }
+  ngOnInit() {
+    this.title = 'Hello-app';
+    this.message = 'false,false,false';
+    this.nowStyle = {
+      'border-style':'',
+      'border-width':'',
+      'border-color':''
+    };
+  }
 
+  check(in1, in2, in3) {
+    this.nowStyle['border-style'] = in1;
+    this.nowStyle['border-width'] = in2 + "px";
+    this.nowStyle['border-color'] = in3;
+    this.message = JSON.stringify(this.nowStyle);
+  }
 }
