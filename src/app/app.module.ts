0d30ee1e9b700c929ada3bc8ac1e96afc0e97712
zenfumi <ys00878@gmail.com>
2021-02-28 13:19:12 +0900

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
@@ -10,7 +11,9 @@ import { HelloComponent } from './hello/hello.component';
     HelloComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [HelloComponent]
--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -9,24 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class HelloComponent implements OnInit {
   title:string;
   message:string;
-  nowStyle:any;
+  text1:string;
 
   constructor() {}
 
   ngOnInit() {
     this.title = 'Hello-app';
-    this.message = 'false,false,false';
-    this.nowStyle = {
-      'border-style':'',
-      'border-width':'',
-      'border-color':''
-    };
-  }
-
-  check(in1, in2, in3) {
-    this.nowStyle['border-style'] = in1;
-    this.nowStyle['border-width'] = in2 + "px";
-    this.nowStyle['border-color'] = in3;
-    this.message = JSON.stringify(this.nowStyle);
+    this.message = 'ngModelを使う';
+    this.text1 = '';
   }
 }
